674b3a8f86ec17651a5c002f49174403a477a2b5
colinjlacy <colin@webcake.co>
2014-12-20 21:17:13 -0600

    [admin] adds basic admin support and some brief admin template customization

--- cms/settings.py
+++ cms/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 ROOT_URLCONF = 'cms.urls'
 
 WSGI_APPLICATION = 'cms.wsgi.application'
