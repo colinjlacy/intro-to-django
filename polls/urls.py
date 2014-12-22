efd87f32faab4de514c1e5f78f4633d49616388d
colinjlacy <colin@webcake.co>
2014-12-21 18:54:52 -0600

    [forms] adds voting interactivity to the choice select, revises templating to make use of generic templates

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,13 @@ from polls import views
 
 urlpatterns = patterns('',
     # e.g.: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # e.g.: /polls/5/
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/$', views.DetailsView.as_view(), name='detail'),
 
     # e.g.: /polls/5/results
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 
     # e.g.: /polls/5/vote
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
